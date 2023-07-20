d2c63cc4d7fd0dd2de82192b3d3924720156b28f
Victor Dubeux <adv.victordubeux@gmail.com>
2023-07-20 15:57:00 -0300

    6. Added lines of code
    Inseri algumas linhas de código no script do pause menu, permitindo o ajuste do volume.

--- Assets/Scripts_Pause/PauseManager.cs
+++ Assets/Scripts_Pause/PauseManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class PauseManager : MonoBehaviour
@@ -10,13 +11,20 @@ public class PauseManager : MonoBehaviour
     public GameObject[] Buttons;
 
     [Header("Canvas GameObject:")]
-    public GameObject PauseMenu; 
+    public GameObject PauseMenu;
+
+    public Toggle MusicToggle, SfxToggle;
+    public Slider MusicSlider, SfxSlider;
+
+    //private AudioSource MusicSource, SfxSource;
     #endregion
 
     private bool _pauseEnabled = false;
 
     void Start()
     {
+        //MusicSource = GameObject.FindGameObjectWithTag("MusicSource").GetComponent<AudioSource>();
+        //SfxSource = GameObject.FindGameObjectWithTag("SfxSource").GetComponent<AudioSource>();
         PauseMenu.SetActive(false);
     }
 
@@ -24,6 +32,10 @@ public class PauseManager : MonoBehaviour
     {
         OnMouseDown();
         EnablePauseMenu();
+        LoadMusicToggleValue();
+        LoadSfxToggleValue();
+        LoadMusicValue();
+        LoadSfxValue();
     }
 
     #region OnMouseDown 
@@ -65,5 +77,84 @@ public class PauseManager : MonoBehaviour
     void OpenMainMenu() 
     {
         
+    }
+
+    public void ToggleMusic()
+    {
+        //AudioManager.Instance.ToggleMusic();
+        if (MusicToggle.isOn == true)
+        {
+            PlayerPrefs.SetInt("musicToggleValue", 0);
+            //MusicSource.mute = false;
+        }
+        else if (MusicToggle.isOn == false)
+        {
+            PlayerPrefs.SetInt("musicToggleValue", 1);
+            //MusicSource.mute = true;
+        }
+        LoadMusicToggleValue();
+    }
+
+    void LoadMusicToggleValue()
+    {
+        if (PlayerPrefs.HasKey("musicToggleValue"))
+        {
+            if (PlayerPrefs.GetInt("musicToggleValue") == 0) MusicToggle.isOn = true;
+            else if (PlayerPrefs.GetInt("musicToggleValue") == 1) MusicToggle.isOn = false;
+        }
+        else PlayerPrefs.SetInt("musicToggleValue", 0);
+    }
+
+    public void ToggleSFX()
+    {
+        //AudioManager.Instance.ToggleSFX();
+        if (SfxToggle.isOn == true)
+        {
+            PlayerPrefs.SetInt("sfxToggleValue", 0);
+            //SfxSource.mute = false;
+        }
+        else if (SfxToggle.isOn == false)
+        {
+            PlayerPrefs.SetInt("sfxToggleValue", 1);
+            //SfxSource.mute = true;
+        }
+    }
+
+    void LoadSfxToggleValue()
+    {
+        if (PlayerPrefs.HasKey("sfxToggleValue"))
+        {
+            if (PlayerPrefs.GetInt("sfxToggleValue") == 0) SfxToggle.isOn = true;
+            else if (PlayerPrefs.GetInt("sfxToggleValue") == 1) SfxToggle.isOn = false;
+        }
+        else PlayerPrefs.SetInt("sfxToggleValue", 0);
+    }
+
+    public void MusicVolume()
+    {
+        //AudioManager.Instance.MusicVolume(MusicSlider.value);
+        float musicVolumeValue = MusicSlider.value;
+        PlayerPrefs.SetFloat("MusicVolumeValue", musicVolumeValue);
+        LoadMusicValue();
+    }
+
+    void LoadMusicValue()
+    {
+        float musicVolumeValue = PlayerPrefs.GetFloat("MusicVolumeValue");
+        MusicSlider.value = musicVolumeValue;
+    }
+
+    public void SFXVolume()
+    {
+        //AudioManager.Instance.SFXVolume(SfxSlider.value);
+        float sfxVolumeValue = SfxSlider.value;
+        PlayerPrefs.SetFloat("SfxVolumeValue", sfxVolumeValue);
+        LoadMusicValue();
+    }
+
+    void LoadSfxValue()
+    {
+        float sfxVolumeValue = PlayerPrefs.GetFloat("SfxVolumeValue");
+        SfxSlider.value = sfxVolumeValue;
     }
 }
